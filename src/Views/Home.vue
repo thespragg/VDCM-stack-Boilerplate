31d590c88a3bb32ea165eb6b71dc65eb64481930
Alistair Spragg <alistair.spragg@gmail.com>
2020-03-22 20:59:52 +0000

    Implemented router & store

---
+++ src/Views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        HomePage
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import router from './router';
+import { store } from './store/store'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
