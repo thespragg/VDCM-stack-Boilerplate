77f3cb0e8c4764082f8b837431c9d78c9882f7f8
Thespragg <alistair@aspragg.me>
2020-03-23 16:10:36 +0000

    Started implementing user logins

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Konscious.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
+using server.Models;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace server.Controllers
 {
@@ -11,5 +11,49 @@ namespace server.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        [HttpPost]
+        public bool Login(User user)
+        {
+            return true;
+        }
+
+        [HttpPost("Register")]
+        public bool Register(NewUser user)
+        {
+            //Check email isnt taken
+
+            //Store this
+            var salt = CreateSalt();
+            //Store this
+            var hash = HashPassword(user.Password, salt);
+
+            return true;
+        }
+
+        private byte[] CreateSalt()
+        {
+            var buffer = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(buffer);
+            return buffer;
+        }
+
+        private byte[] HashPassword(string password, byte[] salt)
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+            argon2.Salt = salt;
+            argon2.DegreeOfParallelism = 8;
+            argon2.Iterations = 4;
+            argon2.MemorySize = 1024 * 1024;
+
+            return argon2.GetBytes(16);
+        }
+
+        private bool VerifyHash(string password, byte[] salt, byte[] hash)
+        {
+            var newHash = HashPassword(password, salt);
+            return hash.SequenceEqual(newHash);
+        }
     }
 }
---
+++ server/Models/NewUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class NewUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
