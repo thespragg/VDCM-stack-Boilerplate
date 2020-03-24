7f432814b13541090975bc0e8d7a459f7d438787
Thespragg <alistair@aspragg.me>
2020-03-24 12:20:27 +0000

    Moved authentication logic into the router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import jwt from 'jsonwebtoken';
 
 Vue.use(VueRouter);
 
@@ -8,5 +9,24 @@ const router = new VueRouter({
   mode: 'history',
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const user = localStorage.token;
+
+  if (authRequired && !user) {
+    return next('/login');
+  }
+  
+  if (user) {
+    let token = jwt.decode(user);
+    if(Date.now() > token.exp * 1000){
+      return next('/login')
+    }
+  }
+
+  next();
+})
 
 export default router;
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,8 +1,9 @@
 const Home = () => import('../Views/Home.vue');
+const Login = () => import('../Views/Login');
+
 export default [
-    {
-        name: "Dashboard",
-      path: '/',
-      component: Home
-    }
+    { name: "Home", path: '/', component: Home},
+    { name: "Login", path: '/login', component:Login},
+
+    { name:'Default', path: '*', redirect: '/'}
 ]
